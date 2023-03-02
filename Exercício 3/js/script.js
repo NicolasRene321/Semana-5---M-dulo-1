79939519affb0978cade05123a13b4b78413de5a
Nícolas Renê <nicolastonete@gmail.com>
2023-03-02 16:22:48 -0300

    Exercício 3

---
+++ Exercício 3/js/script.js
@@ -0,0 +1,62 @@
+var titulo = document.querySelector('#titulo');
+var nota = document.querySelector('#nota');
+var duracaoMin = document.querySelector('#duracaoMin');
+var botao = document.querySelector('#botao');
+var filme = {};
+
+botao.addEventListener('click', cadastrarFilme);
+
+function cadastrarFilme() {
+    if(titulo.value === filme.titulo){
+        verificarTitulo();
+    }else{
+        filme.titulo = titulo.value ;
+        filme.nota = parseFloat(nota.value);
+        filme.duracao = parseFloat(duracaoMin.value);
+        filme.assistido = false;
+        filme.favoritado = false;
+        console.log(filme);
+        window.alert("Filme adicionado com sucesso!");
+        listarFilme();
+    }
+}
+
+function verificarTitulo() {
+    window.alert("Já possui um filme com esse mesmo título!");
+}
+
+
+
+function listarFilme() {
+    var lista = document.getElementsByTagName('article')[0];
+    var card = document.createElement('div');
+    var imagem = document.createElement('img');
+    var titulo1 = document.createElement('h3');
+    var nota1 = document.createElement('h4');
+    var duracao1 = document.createElement('h4');
+
+    card.classList = 'card';
+    lista.appendChild(card);
+    card.appendChild(imagem);
+    card.appendChild(titulo1);
+    card.appendChild(nota1);
+    card.appendChild(duracao1);
+
+    imagem.src = "../img/roloFilme.jpg";
+    imagem.style.width = '230px';
+    card.style.display = 'inline-block';
+    card.style.marginLeft = '70px';
+    card.style.marginTop = '60px';
+    card.style.border = 'solid';
+    card.style.borderColor = 'black';
+    card.style.backgroundColor = '#e6b33d';
+    titulo1.style.marginLeft = '5px';
+    nota1.style.marginLeft = '5px';
+    duracao1.style.marginLeft = '5px';
+
+    titulo1.innerHTML = "Título: " + titulo.value;
+    nota1.innerHTML = "Nota: " + nota.value;
+    duracao1.innerHTML = "Duração: " + duracaoMin.value + " minutos";
+
+    console.log(card);
+}
